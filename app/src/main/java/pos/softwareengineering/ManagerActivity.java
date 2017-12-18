9f87ac2b487b47f3bc04559e6926c997b3027503
choigangho <dldlwjddnjs@naver.com>
2017-12-18 22:54:38 +0900

    dii

---
+++ app/src/main/java/pos/softwareengineering/ManagerActivity.java
@@ -0,0 +1,13 @@
+package pos.softwareengineering;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+}
---
+++ app/src/main/java/pos/softwareengineering/SaleInfoActivity.java
@@ -0,0 +1,13 @@
+package pos.softwareengineering;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SaleInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_info);
+    }
+}
