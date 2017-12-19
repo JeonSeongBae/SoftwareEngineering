22d7b7b8c8385fe8b6dd996464e0d5f09af7996d
choigangho <dldlwjddnjs@naver.com>
2017-12-19 23:05:35 +0900

    idisii

--- app/src/main/java/pos/softwareengineering/MainActivity.java
+++ app/src/main/java/pos/softwareengineering/MainActivity.java
@@ -1,13 +1,37 @@
 package pos.softwareengineering;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button paybtn,managerbtn;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        managerbtn = (Button)findViewById(R.id.managerbtn);
+        paybtn = (Button)findViewById(R.id.paybtn);
+        mContext = this;
+        managerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ManagerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        paybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),PaymentActivity.class);
+                startActivityForResult(intent,101);
+            }
+        });
     }
 }
