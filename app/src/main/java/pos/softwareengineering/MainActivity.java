2ca0317425304f2c075a77e6162875ea07fc28b5
JeonSeongBae <tjdqo6042@naver.com>
2017-12-19 23:21:44 +0900

    add upload item

--- app/src/main/java/pos/softwareengineering/MainActivity.java
+++ app/src/main/java/pos/softwareengineering/MainActivity.java
@@ -1,5 +1,6 @@
 package pos.softwareengineering;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(MainActivity.this, RegisterMenuActivity.class);
+
+        startActivity(intent);
     }
 }
