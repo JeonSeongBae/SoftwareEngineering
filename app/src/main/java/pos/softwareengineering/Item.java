ae2ad590886d923d0ee9e7235bdebf2957393002
JeonSeongBae <tjdqo6042@naver.com>
2017-12-19 22:36:21 +0900

    new class item

---
+++ app/src/main/java/pos/softwareengineering/Item.java
@@ -0,0 +1,37 @@
+package pos.softwareengineering;
+
+public class Item {
+    private String category;
+    private String menu;
+    private String price;
+
+    public Item(String category, String menu, String price) {
+        this.category = category;
+        this.menu = menu;
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
